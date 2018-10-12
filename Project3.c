fcf72ccebaa9d6de6476548b839a1dde6e029780
RyanH1234 <hodgsonr1234@gmail.com>
2018-10-12 13:33:47 +0800

    Initial Template/Skeleton Of The Matrix Multiplication Folder, Contains Some Testing Matricies

---
+++ Project3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/*
+	COMPILE : gcc -o Project3 Project3.c [fileName1] [fileName2]
+	RUN : ./Project3.c
+
+*/
+
+void main(int argc, char *argv[])
+{
+
+	printf("This is what argv contains : \n");
+	for(int i = 0; i < argc; i++)
+	{
+		printf("%s ", argv[i]);
+	}
+
+
+	//get command line arguments - there should be two commands with the file name
+	if(argc <= 5)
+	{
+		
+	}
+
+
+	//for each of the file names given
+
+		//open the file with this file name
+
+		//load this file...
+
+		//sort this file using the linux shell command sort
+
+		//write this to a file - matrix1 and matrix2
+
+	// do your fancy stuff...
+
+
+
+
+}
