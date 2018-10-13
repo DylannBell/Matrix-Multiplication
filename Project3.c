c887aee6b8d49bb83d9fa5a42acb43e52035fa1a
DylannBell <44094394+DylannBell@users.noreply.github.com>
2018-10-13 17:10:23 +0800

    Update Project3.c

--- Project3.c
+++ Project3.c
@@ -4,8 +4,10 @@
 
 /*
 	COMPILE : gcc -std=c99 -o Project3 Project3.c [fileName1] [fileName2] [fileSize1] [fileSize2]
-	RUN [LINUX] : ./Project3.c test1.txt test2.txt 10 10
-	RUN [WINDOWS] : Project3.c test1.txt test2.txt 10 10
+	RUN [LINUX] : ./Project3.c test1.mtx test1.mtx 5 5
+	RUN [WINDOWS] : Project3.c test1.mtx test1.mtx 5 5
+
+	BOTH MATRIX FILES MUST BE ORDERED BY ROWS
 */
 
 /*
@@ -14,29 +16,41 @@
 	noOfRows - the number of rows in the file, computed elsewhere
 	return - a 2D matrix with all the information from the file
 */
-void fileToMatrix(FILE *fp, int noOfRows, float** matrix)
+
+// I think a struct may be better so we can have ints and floats
+// To access the values its just matrix1[i].row etc
+struct SparseRow {
+	int row;
+	int col;
+	float value;
+};
+
+void fileToMatrix(FILE *fp, int noOfRows, struct SparseRow *matrix)
 {
 	//if you can't open the file - throw an error
 	if(fp == NULL)
 	{
 		printf("Error: cannot open file");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	
 	//for each of the lines within the file - split up the different components based on whitespace
 	char line [ 128 ]; 
-	int row = 0;
+	int lineNumber = 0;
+
 	while (fgets(line, sizeof line, fp) != NULL )
 	{	
-		matrix[row][1] = atof(strtok(line, " "));
-		matrix[row][2] = atof(strtok(line, " "));
-		matrix[row][3] = atof(strtok(line, " "));
-		
-		printf("ROW : %f COLUMN : %f VALUE : %f \n", matrix[row][1], matrix[row][2], matrix[row][3]);
-		
-		row++;
-	}
 
+		// Updated to NULL for 2nd and 3rd strok because that's how you get the next token
+		matrix[lineNumber].row = atoi(strtok(line, " "));
+		matrix[lineNumber].col = atoi(strtok(NULL, " "));
+		matrix[lineNumber].value = atof(strtok(NULL, " "));
+		
+		printf("ROW : %d COLUMN : %d VALUE : %f \n", 
+			matrix[lineNumber].row, matrix[lineNumber].col, matrix[lineNumber].value);
+
+		lineNumber++;
+	}
 }
 
 void main(int argc, char *argv[])
@@ -60,44 +74,19 @@ void main(int argc, char *argv[])
 	FILE *fp1 = fopen (file1, "r");
 	FILE *fp2 = fopen(file2, "r");
 	
-	/**
-	
-		https://stackoverflow.com/questions/3911400/how-to-pass-2d-array-matrix-in-a-function-in-c
-	
-	**/
-	
-	//store the file values into a matrix
-	float **matrix1 = malloc(noRows1 * sizeof *matrix1);
-	for(int i = 0; i < noRows1; i++)
-	{
-		matrix1[i] = malloc(3 * sizeof *matrix1[i]);
-	}
+	// Create structs to hold sparse matrix
+	struct SparseRow matrix1[noRows1];
+	struct SparseRow matrix2[noRows2];
+
+	// Load the file into the matrix
 	fileToMatrix(fp1, noRows1, matrix1);
-	
-	float **matrix2 = malloc(noRows2 * sizeof *matrix2);
-	for(int j = 0; j < noRows2; j++)
-	{
-		matrix2[j] = malloc(3 * sizeof *matrix2[j]);
-	}
-	fileToMatrix(fp1, noRows2, matrix2);
-	
-	
-	//deallocate memory for both arrays
-	for (int i=0; i<noRows1; i++)
-	{
-		free(matrix1[i]);
-	}
-	free(matrix1);
-	
-	for (int i=0; i<noRows2; i++)
-	{
-		free(matrix2[i]);
-	}
-	free(matrix2);
+	fileToMatrix(fp2, noRows2, matrix1);	
 	
 	//close both files
 	fclose(fp1);
 	fclose(fp2);
+
+	//http://www.mathcs.emory.edu/~cheung/Courses/561/Syllabus/3-C/sparse.html
 	
 
 
